e442a9868acc521c813487b95623e3eea54e5bff
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2024-12-10 14:56:11 +0100

    change types

--- src/types/nuxt-hooks.d.ts
+++
@@ -1,6 +0,0 @@
-import type { NuxtHooks as I18nHooks } from '@nuxtjs/i18n';
-import type { NuxtHooks as TailwindHooks } from '@nuxtjs/tailwindcss';
-
-declare module '@nuxt/schema' {
-  interface NuxtHooks extends I18nHooks, TailwindHooks {}
-}
